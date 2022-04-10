67575122b300f4804227bab944791d6d473f82e1
Jack Whitham <jack.d.whitham@gmail.com>
2022-04-10 13:41:53 +0100

    clear signal

--- scope2.py
+++ scope2.py
@@ -4,7 +4,7 @@ import collections
 # Read raw data
 times = []
 analogue = []
-for line in open("left24khz.csv", "rt"):
+for line in open("signal.csv", "rt"):
     fields = line.rstrip().split(",")
     try:
         t = float(fields[0])
@@ -15,14 +15,15 @@ for line in open("left24khz.csv", "rt"):
     times.append(t)
     analogue.append(v)
 
-period = 1e-6 * ((times[-1] - times[0]) / (len(times) - 1))
-freq = 1.0 / period
-print("period", period, "seconds")
-print("frequency", freq, "Hz")
+osc_period = 1e-6 * ((times[-1] - times[0]) / (len(times) - 1))
+osc_freq = 1.0 / osc_period
+print("Oscilloscope clock period {:1.3f} microseconds".format(osc_period * 1e6))
+print("Oscilloscope clock frequency {:1.3f} MHz".format(osc_freq / 1e6))
 
 # Digitise
-threshold1 = 20
-threshold0 = 10
+average = sum(analogue) / len(analogue)
+threshold1 = average * 1.1
+threshold0 = average / 1.1
 
 digital = []
 state = False
@@ -34,7 +35,7 @@ for i in range(len(analogue)):
 
     digital.append(state)
 
-# Get pulse width
+# Get S/PDIF clock pulse width
 hold_time = -len(digital)
 hold_time_histogram = collections.defaultdict(lambda: 0)
 max_hold_time = 0
@@ -46,9 +47,9 @@ for i in range(1, len(digital)):
         max_hold_time = max(hold_time, max_hold_time)
         hold_time = 1
 
-for (hold_time, count) in sorted(hold_time_histogram.items()):
-    if hold_time > 0:
-        print("hold_time width {} has count {}".format(hold_time, count))
+#for (hold_time, count) in sorted(hold_time_histogram.items()):
+#    if hold_time > 0:
+#        print("hold_time width {} has count {}".format(hold_time, count))
 
 best_score = -1
 best_hold_time = 1
@@ -59,13 +60,20 @@ for hold_time in range(1, max_hold_time):
             + hold_time_histogram.get(hold_time * 2 + 1, 0))
 
     score = peak0 + peak1
-    print("hold time {} has score {}".format(hold_time, score))
+    #print("hold time {} has score {}".format(hold_time, score))
     if score > best_score:
         best_score = score
         best_hold_time = hold_time
 
+# What's the S/PDIF bit rate? (Time to send a single bit)
+spdif_period = best_hold_time * osc_period * 2
+spdif_freq = 1.0 / spdif_period
+print("S/PDIF clock frequency {:1.3f} MHz".format(spdif_freq / 1e6))
+
+# Thresholds for longer pulses
 width1 = ((best_hold_time * 2) - 1)
 width2 = ((best_hold_time * 3) - 1)
+print("width0", best_hold_time)
 print("width1", width1)
 print("width2", width2)
 
@@ -91,14 +99,15 @@ for i in range(1, len(digital)):
                 skip = False
         pulse = 1
 
+print("Packets", len(packets))
 
 with open("packet.txt", "wt") as fd:
     j = 0
     for packet in packets:
-        fd.write("({:1.3f} -> {:-3d}) ".format(j * period * 1e6, len(packet)))
+        fd.write("({:1.3f} -> {:-3d}) ".format(j * osc_period * 1e6, len(packet)))
         audio = 0
         if len(packet) > 28:
-            for data in packet[0:24]:
+            for data in reversed(packet[11:28]):
                 audio = audio << 1
                 if data:
                     audio |= 1
