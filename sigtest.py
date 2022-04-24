298fe2a5d9a6d26c5a0907cb23b6749e1b22780c
Jack Whitham <jack.d.whitham@gmail.com>
2022-04-24 18:24:31 +0100

    search for markers

--- sigtest.py
+++ sigtest.py
@@ -1,5 +1,6 @@
 
 import sys
+import typing
 from picoscope_decode import picoscope_decode
 from spdif_decode import biphase_mark_decode, spdif_decode, AudioData
 
@@ -24,7 +25,37 @@ MARKER_MASK = 0xfff000
 REPEAT_SIZE = 40
 MASK_16 = 0xffff00
 
-def examine_packet(audio: AudioData) -> bool:
+def int_conv(unsigned_data: int, bits: int) -> int:
+    assert 0 <= unsigned_data < (1 << bits)
+    if unsigned_data & ((1 << bits) >> 1):
+        return unsigned_data - (1 << bits)
+    else:
+        return unsigned_data
+
+def examine_walking_1s(samples: AudioData, bits: int) -> typing.Tuple[bool, int]:
+    assert bits in (16, 24)
+    shift = 24 - bits
+    incorrect = -1
+    small_error = False
+    for i in range(24):
+        left = 1 << i
+        right = left ^ 0xffffff
+        left_delta = abs(int_conv(left >> shift, bits) - int_conv(samples[i].left >> shift, bits))
+        right_delta = abs(int_conv(right >> shift, bits) - int_conv(samples[i].right >> shift, bits))
+
+        if (left_delta > 1) or (right_delta > 1):
+            # Too much error
+            print("at {} ({}-bit): expect {:06x} {:06x}  got {:06x} {:06x}".format(
+                    i, bits, left, right, samples[i].left, samples[i].right))
+            incorrect = i
+
+        elif (left_delta == 1) or (right_delta == 1):
+            # This small error could be due to rounding
+            small_error = True
+
+    return (small_error, incorrect)
+
+def examine_audio_data(audio: AudioData) -> bool:
     # Find the marker
     marker_position = -1
     for i in range(len(audio)):
@@ -35,10 +66,17 @@ def examine_packet(audio: AudioData) -> bool:
     if marker_position < 0:
         print("Unable to find the {:06x} marker within the audio data".format(
                 MARKER_VALUE))
-        return False
 
-    if len(audio) < REPEAT_SIZE:
-        print("Insufficient samples captured (need at least {})".format(REPEAT_SIZE))
+        for i in range(len(audio)):
+            left = audio[i].left >> 8
+            right = audio[i].right >> 8
+            if ((left >= 300) and (left <= 960) # possible sample rate
+                    and (right <= 0x6543) and (right >= 0x63b0)):
+                scale = right / 0x6543
+                print("Possible marker at position {} with volume level "
+                        "reduced to {:1.3f}: sample rate {:1.0f} Hz ?".format(
+                    i, scale, (left / scale) * 100))
+
         return False
 
     # Rearrange data
@@ -56,19 +94,25 @@ def examine_packet(audio: AudioData) -> bool:
     assert (samples[TRUE_MARKER_POSITION].right & MARKER_MASK) == (MARKER_VALUE & MARKER_MASK)
     print("Sample rate of test data: {} Hz".format((samples[TRUE_MARKER_POSITION].left >> 8) * 100))
 
-    # Check walking 1s
-    incorrect = -1
-    for i in range(24):
-        left = 1 << i
-        right = left ^ 0xffffff
-        if (samples[i].left != left) or (samples[i].right != right):
-            incorrect = i
+    # Check walking 1s (16 bit mode)
+    (small_error, incorrect) = examine_walking_1s(samples, 16)
 
-    print("{} walking 1s are correct ({} exact bits)".format(23 - incorrect, 23 - incorrect))
+    if (incorrect < 0) and not small_error:
+        print("Walking ones are perfectly correct for 16-bit")
 
-    if incorrect >= 8:
-        print("Error in 'walking ones' payload part: signal is not 16-bit clean.")
-        return False
+        (small_error, incorrect) = examine_walking_1s(samples, 24)
+        if (incorrect < 0) and not small_error:
+            print("Walking ones are perfectly correct for 24-bit")
+        elif incorrect < 0:
+            print("Small error detected for 24-bit: not bit-exact")
+        else:
+            print("{} walking 1s are correct ({} exact bits)".format(23 - incorrect, 23 - incorrect))
+
+    elif incorrect < 0:
+        print("Walking ones are correct for 16-bit with at most +/- 1 bit error")
+
+    else:
+        print("{} walking 1s are correct ({} exact bits)".format(23 - incorrect, 23 - incorrect))
 
     # Check third part of the repeating block: 16 bit data (7 samples)
     j = 0
@@ -76,6 +120,8 @@ def examine_packet(audio: AudioData) -> bool:
         left =  (PAYLOAD[j + 0] << 16) | (PAYLOAD[j + 1] << 8)
         right = (PAYLOAD[j + 2] << 16) | (PAYLOAD[j + 3] << 8)
         if (((samples[i].left & MASK_16) != left) or ((samples[i].right & MASK_16) != right)):
+            print("at {} (16-bit): expect {:06x} {:06x}  got {:06x} {:06x}".format(
+                    i, left, right, samples[i].left, samples[i].right))
             print("Error in 16-bit payload part, position {}: signal is not 16-bit clean".format(i))
             return False
         j += 4
@@ -87,8 +133,9 @@ def examine_packet(audio: AudioData) -> bool:
         left =  (PAYLOAD[j + 0] << 16) | (PAYLOAD[j + 1] << 8) | (PAYLOAD[j + 2] << 0)
         right = (PAYLOAD[j + 3] << 16) | (PAYLOAD[j + 4] << 8) | (PAYLOAD[j + 5] << 0)
         if ((samples[i].left != left) or (samples[i].right != right)):
+            print("at {} (24-bit): expect {:06x} {:06x}  got {:06x} {:06x}".format(
+                    i, left, right, samples[i].left, samples[i].right))
             print("Error in 24-bit payload part, position {}: signal is not 24-bit clean".format(i))
-            print("{} walking 1s are correct (< 24 exact bits)".format(23 - incorrect))
             return False
         j += 6
 
@@ -108,11 +155,21 @@ def main() -> None:
     packets = biphase_mark_decode(digital, osc_period)
     (audio, subcode_data) = spdif_decode(packets)
 
+
     print("Audio data received:")
     for sample in audio:
         print("{:06x} {:06x}".format(sample.left, sample.right))
 
-    examine_packet(audio)
+    if len(audio) <= REPEAT_SIZE:
+        print("Insufficient samples captured (need more than {})".format(REPEAT_SIZE))
+        sys.exit(1)
+
+    # remove final sample (may be incomplete)
+    audio.pop()
+
+    # analyse
+    if not examine_audio_data(audio):
+        sys.exit(1)
 
 
 if __name__ == "__main__":
