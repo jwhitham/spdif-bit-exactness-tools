4e30ce949b307479434d727c5e3bbab80dff499f
Jack Whitham <jack.d.whitham@gmail.com>
2022-04-30 15:34:53 +0100

    misidentifying

--- fpga/input_decoder.vhdl
+++ fpga/input_decoder.vhdl
@@ -3,8 +3,6 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-use std.textio.all;
-
 entity input_decoder is
     port (
         data_in         : in std_logic;
@@ -17,16 +15,15 @@ end input_decoder;
 
 architecture structural of input_decoder is
 
-    subtype t_sync_counter is unsigned (0 to 1);
-    subtype t_data_counter is unsigned (0 to 5);
-    subtype t_pending is Natural range 0 to 3;
+    subtype t_sync_counter is unsigned (0 to 2);
+    subtype t_data_counter is unsigned (0 to 3);
 
     constant zero_data_counter  : t_data_counter := (others => '0');
     constant zero_sync_counter  : t_sync_counter := (others => '0');
     constant max_sync_counter   : t_sync_counter := (others => '1');
 
     constant max_pulse_length : t_data_counter := (others => '1');
-    constant min_pulse_length : t_data_counter := to_unsigned (2, t_data_counter'Length);
+    constant min_pulse_length : t_data_counter := to_unsigned (1, t_data_counter'Length);
 
     type t_pulse_length is (SHORT_INVALID, ZERO, ONE, TWO, THREE, LONG_INVALID);
 
@@ -39,21 +36,11 @@ architecture structural of input_decoder is
 begin
 
     process (clock)
-        variable l : line;
     begin
         if clock'event and clock = '1' then
             single_pulse <= '0';
             double_pulse <= '0';
             triple_pulse <= '0';
-            write (l, String'("sync counter = "));
-            write (l, to_integer(sync_counter));
-            write (l, String'(" data_counter = "));
-            write (l, to_integer(data_counter));
-            write (l, String'(" pulse_length = "));
-            write (l, t_pulse_length'Image(pulse_length));
-            write (l, String'(" single_pulse_length = "));
-            write (l, to_integer(single_pulse_length));
-            writeline (output, l);
 
             delay0 <= data_in;
             delay1 <= delay0;
@@ -64,14 +51,11 @@ begin
 
                 if data_counter = max_pulse_length then
                     -- If a pulse is too long, we must resynchronise
-                    write (l, String'("Stable input - max pulse length reached"));
-                    writeline (output, l);
+                    pulse_length <= LONG_INVALID;
                     data_counter <= zero_data_counter;
 
                 elsif data_counter = single_pulse_length then
                     -- reached the measured length of a pulse
-                    write (l, String'("Stable input - measured pulse length reached"));
-                    writeline (output, l);
                     case pulse_length is
                         when ZERO | SHORT_INVALID =>
                             pulse_length <= ONE;
@@ -86,15 +70,15 @@ begin
 
                 elsif data_counter = min_pulse_length then
                     -- reached the minimum length of a pulse
-                    write (l, String'("Stable input - min pulse length reached"));
-                    writeline (output, l);
-                    pulse_length <= ZERO;
-
+                    case pulse_length is
+                        when SHORT_INVALID =>
+                            pulse_length <= ZERO;
+                        when ZERO | ONE | TWO | THREE | LONG_INVALID =>
+                            null;
+                    end case;
                 end if;
             elsif sync_counter = max_sync_counter then
                 -- Transition input when synchronised
-                write (l, String'("Transition input when synchronised"));
-                writeline (output, l);
                 pulse_length <= SHORT_INVALID;
                 data_counter <= zero_data_counter;
 
@@ -103,9 +87,11 @@ begin
                         -- Pulse is too short or too long: resynchronise
                         sync_counter <= zero_sync_counter;
                         single_pulse_length <= max_pulse_length;
-                        write (l, String'("Pulse is bad when synchronised"));
-                        writeline (output, l);
-                    when ZERO | ONE =>
+                    when ZERO =>
+                        -- This pulse may be shorter than the shortest one we saw so far
+                        single_pulse_length <= data_counter;
+                        single_pulse <= '1';
+                    when ONE =>
                         single_pulse <= '1';
                     when TWO =>
                         double_pulse <= '1';
@@ -114,8 +100,6 @@ begin
                 end case;
             else
                 -- Transition input when not synchronised
-                write (l, String'("Transition input when not synchronised"));
-                writeline (output, l);
                 pulse_length <= SHORT_INVALID;
                 sync_counter <= sync_counter + 1;
                 data_counter <= zero_data_counter;
@@ -125,13 +109,9 @@ begin
                         -- Pulse is too short or too long to be usable: resynchronise
                         sync_counter <= zero_sync_counter;
                         single_pulse_length <= max_pulse_length;
-                        write (l, String'("Pulse is bad when not synchronised"));
-                        writeline (output, l);
                     when ZERO =>
-                        -- This pulse is shorter than the shortest one we saw so far
+                        -- This pulse may be shorter than the shortest one we saw so far
                         single_pulse_length <= data_counter;
-                        write (l, String'("Pulse is shorter than previous"));
-                        writeline (output, l);
                     when ONE | TWO | THREE =>
                         -- This is a stable measurement
                         null;
--- fpga/test_top_level.vhdl
+++ fpga/test_top_level.vhdl
@@ -2,6 +2,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+use std.textio.all;
+
 entity test_top_level is
 end test_top_level;
 
@@ -43,8 +45,31 @@ begin
                   triple_pulse => triple_pulse);
 
     printer : process
+        variable l : line;
+
+        function conv (x : std_logic) return Integer is
+        begin
+            if x = '1' then
+                return 1;
+            else
+                return 0;
+            end if;
+        end conv;
+
     begin
-        wait until done = '1';
+        while done = '0' loop
+            wait until clock'event;
+            if clock = '1' then
+                write (l, conv (data));
+                write (l, string'(" --> "));
+                write (l, conv (single_pulse));
+                write (l, string'(" "));
+                write (l, conv (double_pulse));
+                write (l, string'(" "));
+                write (l, conv (triple_pulse));
+                writeline (output, l);
+            end if;
+        end loop;
         wait;
     end process printer;
 
