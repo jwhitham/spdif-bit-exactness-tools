c0982b7371f9d51006de7d98522c77cfbd8104a7
Jack Whitham <jack.d.whitham@gmail.com>
2022-06-17 19:44:01 +0100

    reduce propagation delay through clock divider

--- fpga/clock_regenerator.vhdl
+++ fpga/clock_regenerator.vhdl
@@ -24,16 +24,17 @@ architecture structural of clock_regenerator is
     -- Count the number of clock_in cycles required for 2**num_packets_log_2 packets
     -- Use this to make a fixed point clock divider to convert clock_in to strobe_out
     subtype t_their_packets is unsigned ((num_packets_log_2 - 1) downto 0);
-    subtype t_my_clocks is unsigned ((counter_bits - 1) downto 0);
+    subtype t_clock_counter is unsigned ((counter_bits - 1) downto 0);
     constant zero_packets       : t_their_packets := (others => '0');
-    constant zero_clocks        : t_my_clocks := (others => '0');
+    constant zero_clocks        : t_clock_counter := (others => '0');
 
     signal their_packets        : t_their_packets := zero_packets;
-    signal my_clocks            : t_my_clocks := (others => '0');
-    signal my_clocks_done       : t_my_clocks := (others => '0');
+    signal clock_counter        : t_clock_counter := (others => '0');
+    signal clock_interval       : t_clock_counter := (others => '0');
+    signal divisor_subtract     : t_clock_counter := (others => '0');
 
-    signal divisor              : t_my_clocks := (others => '0');
-    constant fixed_point_one    : t_mY_clocks := (fixed_point_bits => '1', others => '0');
+    signal divisor              : t_clock_counter := (others => '0');
+    constant fixed_point_one    : t_clock_counter := (fixed_point_bits => '1', others => '0');
 
     type t_measurement_state is (START, IN_HEADER_1, IN_HEADER_2, IN_HEADER_3, IN_BODY);
     signal measurement_state    : t_measurement_state := START;
@@ -45,18 +46,21 @@ architecture structural of clock_regenerator is
     signal packet_start_strobe  : std_logic := '0';
 
     signal strobe_gen           : std_logic := '0';
+
+    type t_output_state is (RESET, ADD, COMPARE_SUBTRACT);
+    signal output_state         : t_output_state := RESET;
 begin
     process (clock_in)
     begin
         if clock_in'event and clock_in = '1' then
-            my_clocks <= my_clocks + 1;
+            clock_counter <= clock_counter + 1;
             packet_start_strobe <= '0';
             case measurement_state is
                 when START =>
                     -- wait for the start of a new packet
-                    my_clocks <= zero_clocks + 1;
+                    clock_counter <= zero_clocks + 1;
                     their_packets <= zero_packets;
-                    my_clocks_done <= zero_clocks;
+                    clock_interval <= zero_clocks;
                     sync_gen <= '0';
                     if pulse_length_in = "11" then
                         measurement_state <= IN_HEADER_1;
@@ -82,8 +86,8 @@ begin
                     if pulse_length_in = "11" then
                         if their_packets = zero_packets then
                             -- counting is complete
-                            my_clocks_done <= my_clocks;
-                            my_clocks <= zero_clocks + 1;
+                            clock_interval <= clock_counter srl 1;
+                            clock_counter <= zero_clocks + 1;
                             sync_gen <= '1';
                         end if;
                         -- back to the header
@@ -99,31 +103,40 @@ begin
     end process;
 
     sync_out <= sync_gen;
+    divisor_subtract <= divisor - clock_interval;
 
     process (clock_in)
     begin
         if clock_in'event and clock_in = '1' then
             strobe_out <= '0';
 
-            if sync_gen = '0' then
-                -- Do nothing while waiting for the measurement stage to synchronise
-                divisor <= (others => '0');
-                clock_count <= (others => '0');
-            elsif packet_start_strobe = '1' then
-                -- Output: start the packet with a clock tick
-                divisor <= (others => '0');
-                clock_count <= (others => '1');
-                strobe_out <= '1';
-            elsif clock_count /= zero_clock_count then
-                -- Output: wait for the right time to send another clock tick
-                if divisor < my_clocks_done then
+            case output_state is
+                when RESET =>
+                    -- Do nothing while waiting for synchronisation and the start of a packet
+                    divisor <= (others => '0');
+                    clock_count <= (others => '1');
+                    if packet_start_strobe = '1' and sync_gen = '1' then
+                        -- Output: start the packet with a clock tick
+                        strobe_out <= '1';
+                        output_state <= ADD;
+                    end if;
+                when ADD =>
+                    -- Advance
                     divisor <= divisor + fixed_point_one;
-                else
-                    divisor <= divisor + fixed_point_one - my_clocks_done;
-                    strobe_out <= '1';
-                    clock_count <= clock_count - 1;
-                end if;
-            end if;
+                    output_state <= COMPARE_SUBTRACT;
+                    if clock_count = zero_clock_count then
+                        output_state <= RESET;
+                    end if;
+                when COMPARE_SUBTRACT =>
+                    -- Output: wait for the right time to send another clock tick
+                    if divisor_subtract (divisor_subtract'Left) = '0' then
+                        -- divisor >= clock_interval
+                        divisor <= divisor_subtract;
+                        strobe_out <= '1';
+                        clock_count <= clock_count - 1;
+                    end if;
+                    output_state <= ADD;
+            end case;
         end if;
     end process;
 
