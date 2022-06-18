6d36a3f4e8c8111556f4658381621c8ee33e04a4
Jack Whitham <jack.d.whitham@gmail.com>
2022-06-18 17:07:04 +0100

    check for correct behaviour with large values

--- fpga/test_compressor.vhdl
+++ fpga/test_compressor.vhdl
@@ -37,6 +37,8 @@ architecture test of test_compressor is
     signal peak_level_out   : std_logic_vector (23 downto 0) := (others => '0');
     signal sync_in          : std_logic := '0';
     signal sync_out         : std_logic := '0';
+    signal sample_counter   : Natural := 0;
+    signal clock_counter    : Natural := 0;
 
     constant clock_period   : Time := 10.0 ns;
     constant sample_period  : Time := 20.0 us;
@@ -59,8 +61,10 @@ begin
     process
     begin
         -- 100MHz clock (one clock every 10 nanoseconds)
+        clock_counter <= 0;
         while done /= '1' loop
             clock <= '1';
+            clock_counter <= clock_counter + 1;
             wait for (clock_period / 2.0);
             clock <= '0';
             wait for (clock_period / 2.0);
@@ -73,9 +77,11 @@ begin
         -- Audio data arriving at 50kHz sample rate (one sample per channel every 20 microseconds)
         left_strobe_in <= '0';
         right_strobe_in <= '0';
+        sample_counter <= 0;
         wait for clock_period;
         while done /= '1' loop
             left_strobe_in <= '1';
+            sample_counter <= sample_counter + 1;
             wait for clock_period;
             left_strobe_in <= '0';
             wait for ((sample_period / 2.0) - clock_period);
@@ -101,7 +107,7 @@ begin
 
     process
         variable l          : line;
-        variable counter    : Natural := 0;
+        variable start      : Natural := 0;
         variable counter2   : Natural := 0;
         variable previous   : std_logic_vector (15 downto 0) := (others => '0');
     begin
@@ -112,51 +118,45 @@ begin
             wait for 1 us;
             -- filling stage: leave reset
             sync_in <= '1';
-            counter := 0;
-            while sync_out = '0' loop
-                wait until clock'event and clock = '1';
-                counter := counter + 1;
-                if counter > Natural (1e9) then
-                    write (l, String'("Never synchronised!"));
-                    writeline (output, l);
-                    assert False;
-                    exit all_proc;
-                end if;
-            end loop;
+            start := sample_counter;
+            wait until sync_out = '1';
             write (l, String'("Synchronised after "));
-            write (l, counter);
-            write (l, String'(" ticks"));
+            write (l, sample_counter - start);
+            write (l, String'(" samples"));
             writeline (output, l);
             write (l, String'("Peak level register value "));
             write (l, to_integer (unsigned (peak_level_out)));
             writeline (output, l);
-
+            
             -- Synchronisation is expected after around 500 samples
-            assert counter >= (500 * sample_period / clock_period);
-            assert counter <= (512 * sample_period / clock_period);
+            assert (sample_counter - start) >= 500;
+            assert (sample_counter - start) < 512;
 
             -- Wait for output data
-            counter := 0;
-            counter2 := 0;
-            previous := x"1234";
-            while counter < Natural (1e9) and counter2 < 10 loop
-                wait until clock'event and clock = '1';
-                if left_strobe_out = '1' then
-                    if data_out /= previous then
-                        write (l, String'("Data out changed to "));
-                        write (l, to_integer (signed (data_out)));
-                        write (l, String'(" at time "));
-                        write (l, counter);
-                        write (l, String'(" peak level "));
-                        write (l, Real (to_integer (unsigned (peak_level_out))) / 256.0);
-                        writeline (output, l);
-                        previous := data_out;
-                        counter2 := counter2 + 1;
-                        -- counter := 0;
-                    end if;
-                end if;
-                counter := counter + 1;
-            end loop;
+            wait until left_strobe_out'event and left_strobe_out = '1';
+            assert data_out = x"7fff" or data_out = x"8000";
+
+            -- Check period of left output strobe
+            start := clock_counter;
+            wait until left_strobe_out'event and left_strobe_out = '1';
+            assert (clock_counter - start) = (sample_period / clock_period);
+
+            -- Wait for right channel data and check that right strobe
+            -- occurs 180 degrees out of phase with the left strobe
+            start := clock_counter;
+            wait until right_strobe_out'event and right_strobe_out = '1';
+            assert (clock_counter - start) = ((sample_period / 2.0) / clock_period);
+
+            -- Check sample values and square wave period
+            wait until left_strobe_out'event and left_strobe_out = '1' and data_out /= x"7fff";
+            start := sample_counter;
+            assert data_out = x"8000";
+            wait until left_strobe_out'event and left_strobe_out = '1' and data_out /= x"8000";
+            assert data_out = x"7fff";
+            wait until left_strobe_out'event and left_strobe_out = '1' and data_out /= x"7fff";
+            assert data_out = x"8000";
+            assert (sample_counter - start) = (square_wave_period / sample_period);
+
 
         end loop all_proc;
         done <= '1';
