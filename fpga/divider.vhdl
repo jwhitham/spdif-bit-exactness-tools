f730fc31d416584c3f7257bca87066a0d6ab1502
Jack Whitham <jack.d.whitham@gmail.com>
2022-06-19 11:56:55 +0100

    extend for signed division (crude implementation)

--- fpga/divider.vhdl
+++ fpga/divider.vhdl
@@ -1,3 +1,13 @@
+-- Signed / unsigned divider with configurable width:
+--
+--   result = top / bottom
+--
+-- The divider does not report an error in these conditions:
+-- (a) If bottom = 0 the result is undefined.
+-- (b) If top / bottom does not fit within top_width bits, the result is undefined.
+--
+-- (b) happens with signed division, when top = minimum and bottom = -1,
+-- because the result is -minimum which can't be encoded.
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -6,7 +16,8 @@ use ieee.numeric_std.all;
 entity divider is
     generic (
         top_width    : Natural;
-        bottom_width : Natural);
+        bottom_width : Natural;
+        is_unsigned  : Boolean);
     port (
         top_value_in    : in std_logic_vector (top_width - 1 downto 0);
         bottom_value_in : in std_logic_vector (bottom_width - 1 downto 0);
@@ -30,6 +41,7 @@ architecture structural of divider is
     signal subtracted   : t_wide := (others => '0');
     signal result       : std_logic_vector (top_width - 1 downto 0) := (others => '0');
     signal state        : t_state := IDLE;
+    signal invert       : std_logic := '0';
 
 begin
     subtracted <= top - bottom;
@@ -43,15 +55,37 @@ begin
             finish_out <= '0';
 
             if start_in = '1' then
-                top (top_width - 1 downto 0) <= unsigned (top_value_in);
+                if is_unsigned or top_value_in (top_width - 1) = '0' then
+                    -- positive or unsigned
+                    top (top_width - 1 downto 0) <= unsigned (top_value_in);
+                else
+                    -- negative
+                    top (top_width - 1 downto 0) <= unsigned (0 - signed (top_value_in));
+                end if;
+
                 bottom (top_width - 2 downto 0) <= (others => '0');
-                bottom (bottom'Left - 1 downto top_width - 1) <= unsigned (bottom_value_in);
+                if is_unsigned or bottom_value_in (bottom_width - 1) = '0' then
+                    -- positive or unsigned
+                    bottom (bottom'Left - 1 downto top_width - 1) <= unsigned (bottom_value_in);
+                else
+                    -- negative
+                    bottom (bottom'Left - 1 downto top_width - 1) <= unsigned (0 - signed (bottom_value_in));
+                end if;
+
+                invert <= '0';
+                if (not is_unsigned) and
+                        (top_value_in (top_width - 1) /= bottom_value_in (bottom_width - 1)) then
+                    invert <= '1';
+                end if;
                 state <= 0;
 
             elsif state /= IDLE then
                 state <= state + 1;
                 if state = FINISHED then
                     -- when finished
+                    if invert = '1' then
+                        result <= std_logic_vector (unsigned (result) + 1);
+                    end if;
                     finish_out <= '1';
                 else
                     -- when dividing
@@ -61,10 +95,10 @@ begin
                     if subtracted (subtracted'Left) = '0' then
                         -- subtraction did not result in overflow
                         top (top_width - 1 downto 0) <= subtracted (top_width - 1 downto 0);
-                        result (0) <= '1';
+                        result (0) <= '1' xor invert;
                     else
                         -- subtraction resulted in overflow
-                        result (0) <= '0';
+                        result (0) <= '0' xor invert;
                     end if;
                 end if;
             end if;
--- fpga/test_divider.vhdl
+++ fpga/test_divider.vhdl
@@ -13,7 +13,8 @@ architecture test of test_divider is
     component divider is
         generic (
             top_width    : Natural;
-            bottom_width : Natural);
+            bottom_width : Natural;
+            is_unsigned  : Boolean);
         port (
             top_value_in    : in std_logic_vector (top_width - 1 downto 0);
             bottom_value_in : in std_logic_vector (bottom_width - 1 downto 0);
@@ -24,15 +25,29 @@ architecture test of test_divider is
         );
     end component divider;
 
+    type t_test is record
+        top_width       : Natural;
+        bottom_width    : Natural;
+        is_unsigned     : Boolean;
+    end record;
+
+    type t_test_table is array (Positive range <>) of t_test;
+
+    constant test_table     : t_test_table :=
+        ((4, 8, true), (8, 4, true), (6, 6, true),
+         (4, 4, false), (8, 4, false), (4, 8, false),
+         (4, 1, false), (4, 1, true), (1, 4, false), (1, 4, true));
+    constant num_tests      : Natural := test_table'Length;
     signal clock            : std_logic := '0';
-    signal done             : std_logic_vector (0 to 3) := (others => '0');
+    signal done             : std_logic_vector (0 to num_tests) := (others => '0');
+
 
 begin
     process
     begin
         done (0) <= '1';
         wait for 500 ns;
-        while done (3) /= '1' loop
+        while done (num_tests) /= '1' loop
             clock <= '1';
             wait for 500 ns;
             clock <= '0';
@@ -41,28 +56,61 @@ begin
         wait;
     end process;
 
-    dtest : for part in 1 to 3 generate
+    dtest : for part in test_table'Range generate
 
-        function get_top_width (p : Natural) return Natural is
+
+        constant top_width      : Natural := test_table (part).top_width;
+        constant bottom_width   : Natural := test_table (part).bottom_width;
+        constant is_unsigned    : Boolean := test_table (part).is_unsigned;
+
+        function top_start return Integer is
         begin
-            case p is
-                when 1 =>       return 4;
-                when 2 =>       return 8;
-                when others =>  return 6;
-            end case;
-        end get_top_width;
+            if is_unsigned then
+                return 0;
+            else
+                return - Integer (2 ** (top_width - 1));
+            end if;
+        end top_start;
 
-        function get_bottom_width (p : Natural) return Natural is
+        function bottom_start return Integer is
         begin
-            case p is
-                when 1 =>       return 8;
-                when 2 =>       return 4;
-                when others =>  return 6;
-            end case;
-        end get_bottom_width;
+            if is_unsigned then
+                return 0;
+            else
+                return - Integer (2 ** (bottom_width - 1));
+            end if;
+        end bottom_start;
 
-        constant top_width : Natural := get_top_width (part);
-        constant bottom_width : Natural := get_bottom_width (part);
+        function top_finish return Integer is
+        begin
+            if is_unsigned then
+                return Integer (2 ** top_width) - 1;
+            else
+                return Integer (2 ** (top_width - 1)) - 1;
+            end if;
+        end top_finish;
+
+        function bottom_finish return Integer is
+        begin
+            if is_unsigned then
+                return Integer (2 ** bottom_width) - 1;
+            else
+                return Integer (2 ** (bottom_width - 1)) - 1;
+            end if;
+        end bottom_finish;
+
+        function convert (value : Integer; size : Natural) return std_logic_vector is
+        begin
+            if is_unsigned then
+                assert 0 <= value;
+                assert value <= ((2 ** size) - 1);
+                return std_logic_vector (to_unsigned (Natural (value), size));
+            else
+                assert (- Integer (2 ** (size - 1))) <= value;
+                assert value <= ((2 ** (size - 1)) - 1);
+                return std_logic_vector (to_signed (value, size));
+            end if;
+        end convert;
 
         signal top_value        : std_logic_vector (top_width - 1 downto 0);
         signal bottom_value     : std_logic_vector (bottom_width - 1 downto 0);
@@ -72,7 +120,9 @@ begin
 
     begin
         d : divider
-            generic map (top_width => top_width, bottom_width => bottom_width)
+            generic map (top_width => top_width,
+                         bottom_width => bottom_width,
+                         is_unsigned => is_unsigned)
             port map (
                 top_value_in => top_value,
                 bottom_value_in => bottom_value,
@@ -83,6 +133,8 @@ begin
 
         process
             variable l : line;
+            variable expect : Integer;
+            variable undefined : Boolean;
         begin
             done (part) <= '0';
             wait until done (part - 1) = '1';
@@ -92,10 +144,15 @@ begin
             write (l, String'("division test "));
             write (l, part);
             writeline (output, l);
-            outer : for top in 0 to (2 ** top_width) - 1 loop
-                for bottom in 1 to (2 ** bottom_width) - 1 loop
-                    top_value <= std_logic_vector (to_unsigned (top, top_width));
-                    bottom_value <= std_logic_vector (to_unsigned (bottom, bottom_width));
+            outer : for top in top_start to top_finish loop
+                for bottom in bottom_start to bottom_finish loop
+                    top_value <= convert (top, top_width);
+                    bottom_value <= convert (bottom, bottom_width);
+                    expect := 0;
+                    undefined := true;
+
+                    -- Though the result of division by zero is undefined, the
+                    -- divider should still finish.
                     start <= '1';
                     wait for 1 us;
 
@@ -105,15 +162,31 @@ begin
                         wait for 1 us;
                     end loop;
 
-                    if result /= std_logic_vector (to_unsigned (top / bottom, top_width)) then
+                    if bottom /= 0 then
+                        expect := top / bottom;
+                        undefined := false;
+                    end if;
+
+                    if expect = (top_finish + 1) then
+                        -- Result overflow; in this case, the behaviour is undefined.
+                        undefined := true;
+                        assert not is_unsigned;
+                        assert top = top_start;
+                        assert bottom = -1;
+                    else
+                        -- Expected result is in range
+                        assert ((top_start <= expect) and (expect <= top_finish));
+                    end if;
+
+                    if (not undefined) and result /= convert (expect, top_width) then
                         write (l, String'("Division error. Dividing "));
                         write (l, top);
                         write (l, String'(" by "));
                         write (l, bottom);
                         write (l, String'(" should be "));
-                        write (l, top / bottom);
+                        write (l, expect);
                         write (l, String'(" got "));
-                        write (l, to_integer (unsigned (result)));
+                        write (l, to_integer (signed (result)));
                         writeline (output, l);
                         assert False;
                         exit outer;
