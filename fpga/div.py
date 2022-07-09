e1a8bb78dc48467c7dd7ffdcedac884f39f030c4
Jack Whitham <jack.d.whitham@gmail.com>
2022-07-09 12:43:53 +0100

    model: alternate method of dealing with negative numbers

--- fpga/div.py
+++ fpga/div.py
@@ -9,27 +9,36 @@ def div(top_value, bottom_value, width):
     assert bottom_value != 0
     assert abs(top_value) < (1 << width)
     assert abs(bottom_value) < (1 << width)
-    inv = False
-    if bottom_value < 0 and top_value < 0:
-        bottom_value = -bottom_value
-        top_value = -top_value
-    elif bottom_value < 0:
-        bottom_value = -bottom_value
-        inv = True
-    elif top_value < 0:
-        top_value = -top_value
-        inv = True
 
+    top_negative = (top_value < 0)
+    bottom_negative = (bottom_value < 0)
     bottom_value = bottom_value << (width - 1)
     out = 0
     for i in range(width):
         out = out << 1
-        if top_value >= bottom_value:
-            out |= 1
-            top_value -= bottom_value
+        subtract = top_value - bottom_value
+        add = top_value + bottom_value
+
+        if bottom_negative and top_negative:
+            if subtract <= 0:
+                out |= 1
+                top_value = subtract
+        elif bottom_negative:
+            if add >= 0:
+                out |= 1
+                top_value = add
+        elif top_negative:
+            if add <= 0:
+                out |= 1
+                top_value = add
+        else:
+            if subtract >= 0:
+                out |= 1
+                top_value = subtract
+
         bottom_value = bottom_value >> 1
  
-    if inv:
+    if bottom_negative != top_negative:
         out = -out
     return out
 
@@ -45,9 +54,12 @@ for x in range(-test_size, test_size + 1):
                 ex = math.floor(ex)
             else:
                 ex = math.ceil(ex)
+
             if z != ex:
                 print("divide {} by {}: got {} expected {}".format(x, y, z, ex))
                 sys.exit(1)
+            #else:
+            #    print("divide {} by {}: got {} ok".format(x, y, z))
 
 print("ok")
 
