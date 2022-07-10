a049fd3e88b06379eaf3115885efd4fbed36654a
Jack Whitham <jack.d.whitham@gmail.com>
2022-07-10 01:30:20 +0100

    model changes

--- fpga/div.py
+++ fpga/div.py
@@ -2,13 +2,35 @@
 import sys
 import math
 
+
+def serial_0_sub(bottom_value, width):
+    out = 0
+    carry = 1
+    check = -bottom_value
+
+    assert 0 <= bottom_value < (1 << width)
+    bottom_value &= (1 << width) - 1
+    check &= (1 << width) - 1
+
+    for i in range(width):
+        new_bit = (bottom_value & 1) ^ 1
+        bottom_value = bottom_value >> 1
+        new_bit += carry
+        out = out >> 1
+        out |= (new_bit & 1) << (width - 1)
+        carry = (new_bit >> 1) & 1
+    #out -= 1 << width
+    bottom_value = out
+    assert check == out, (check, out)
+    return bottom_value
+
 # The divider will round towards 0 (unlike Python's // operator which rounds towards -inf)
 # Unsigned division is easier
 
 def div(top_value, bottom_value, width):
     assert bottom_value != 0
-    assert abs(top_value) < (1 << width)
-    assert abs(bottom_value) < (1 << width)
+    top_value = conv_from_2s_complement(top_value, width)
+    bottom_value = conv_from_2s_complement(bottom_value, width)
 
     top_negative = (top_value < 0)
     bottom_negative = (bottom_value < 0)
@@ -32,18 +54,35 @@ def div(top_value, bottom_value, width):
  
     if bottom_negative != top_negative:
         out += 1
-        out -= 1 << width
 
-    assert abs(out) < (1 << width)
+    out &= (1 << width) - 1
     return out
+
+def conv_to_2s_complement(value, bits):
+    return ((1 << bits) - 1) & value
+
+def conv_from_2s_complement(value, bits):
+    assert 0 <= value < (1 << bits)
+    if value & (1 << (bits - 1)):
+        value -= 1 << bits
+    return value
 
 print("test")
 bits = 7
-test_size = (1 << bits) - 1
-for x in range(-test_size, test_size + 1):
-    for y in range(-test_size, test_size + 1):
+low = -(1 << (bits - 1)) + 1
+high = (1 << (bits - 1))
+for x in range(low, high):
+    y = serial_0_sub(conv_to_2s_complement(x, bits), bits)
+    assert (conv_to_2s_complement(-x, bits)) == y, (x, y)
+    assert (conv_from_2s_complement(y, bits)) == -x, (x, y)
+    assert (conv_from_2s_complement(conv_to_2s_complement(x, bits), bits) == x)
+
+for x in range(low, high):
+    for y in range(low, high):
         if y != 0:
-            z = div(x, y, bits)
+            z = conv_from_2s_complement(
+                    div(conv_to_2s_complement(x, bits),
+                        conv_to_2s_complement(y, bits), bits), bits)
             ex = x / y
             if ex > 0:
                 ex = math.floor(ex)
