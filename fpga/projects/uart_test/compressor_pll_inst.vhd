ec97908dd0f7281eb0b54b137299f708339b84d6
Jack Whitham <jack.d.whitham@gmail.com>
2022-07-14 18:53:45 +0100

    working at 500kbps

---
+++ fpga/projects/uart_test/compressor_pll_inst.vhd
@@ -0,0 +1,7 @@
+compressor_pll_inst: compressor_pll
+port map(
+          REFERENCECLK => ,
+          PLLOUTCORE => ,
+          PLLOUTGLOBAL => ,
+          RESET => 
+        );
--- fpga/projects/uart_test/uart_test_top.vhdl
+++ fpga/projects/uart_test/uart_test_top.vhdl
@@ -1,4 +1,7 @@
 
+library work;
+use work.all;
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -19,16 +22,24 @@ architecture structural of uart_test_top is
     signal data_out      : std_logic_vector (7 downto 0) := (others => '0');
     signal strobe_out    : std_logic := '0';
     signal ready_out     : std_logic := '0';
+    signal clock         : std_logic := '0';
 
 begin
     tx_to_pic <= '1';
     data_in <= std_logic_vector (unsigned (data_out) + 1);
     strobe_in <= strobe_out;
 
+    pll : entity compressor_pll
+        port map (
+              REFERENCECLK => clk12MHz,
+              RESET => '1',
+              PLLOUTCORE => open,
+              PLLOUTGLOBAL => clock);
+
     pi_uart : entity uart
         generic map (
-            clock_frequency => 12.0e6,
-            baud_rate => 1200.0)
+            clock_frequency => 96.0e6,
+            baud_rate => 500.0e3)
         port map (
             data_in => data_in,
             strobe_in => strobe_in,
@@ -37,7 +48,7 @@ begin
             ready_out => open,
             serial_in => rx_from_rpi,
             serial_out => tx_to_rpi,
-            clock_in => clk12MHz);
+            clock_in => clock);
 
 end structural;
 
