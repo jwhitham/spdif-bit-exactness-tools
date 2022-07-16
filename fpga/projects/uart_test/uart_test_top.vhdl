6003e2b43ccc12ce79752e37fcdd20def7bab3f1
Jack Whitham <jack.d.whitham@gmail.com>
2022-07-16 19:39:00 +0100

    show input level from A-D converters on LEDs

--- fpga/projects/uart_test/uart_test_top.vhdl
+++ fpga/projects/uart_test/uart_test_top.vhdl
@@ -9,19 +9,44 @@ use ieee.numeric_std.all;
 entity uart_test_top is
     port (
         clk12MHz        : in std_logic;
-        tx_to_rpi       : out std_logic := '0';
-        rx_from_rpi     : in std_logic;
         tx_to_pic       : out std_logic := '0';
-        rx_from_pic     : in std_logic);
+        rx_from_pic     : in std_logic;
+        lcol1           : out std_logic := '0';
+        lcol2           : out std_logic := '0';
+        lcol3           : out std_logic := '0';
+        lcol4           : out std_logic := '0';
+        led1            : out std_logic := '0';
+        led2            : out std_logic := '0';
+        led3            : out std_logic := '0';
+        led4            : out std_logic := '0';
+        led5            : out std_logic := '0';
+        led6            : out std_logic := '0';
+        led7            : out std_logic := '0';
+        led8            : out std_logic := '0' );
 end uart_test_top;
 
 architecture structural of uart_test_top is
+
+    constant clock_frequency : Real := 96.0e6;
 
     signal data_from_pic    : std_logic_vector (7 downto 0);
     signal data_to_pic      : std_logic_vector (7 downto 0);
     signal strobe_from_pic  : std_logic;
     signal strobe_to_pic    : std_logic;
     signal clock            : std_logic := '0';
+    signal ad1, ad2         : std_logic_vector (15 downto 0);
+    signal lcols       : std_logic_vector (3 downto 0) := "0000";
+    signal lrows       : std_logic_vector (7 downto 0) := "00000000";
+
+    type t_state is (RESET, WAIT_BETWEEN_REQUESTS,
+                     SEND_REQUEST_1, WAIT_REPLY_1, WAIT_REPLY_2,
+                     SEND_REQUEST_2, WAIT_REPLY_3, WAIT_REPLY_4,
+                     TIMEOUT_ERROR);
+    signal state            : t_state := RESET;
+
+    constant max_countdown  : Natural := Natural (clock_frequency / 10.0);
+    subtype t_countdown is Natural range 0 to max_countdown;
+    signal countdown        : t_countdown := max_countdown;
 
 begin
     pll : entity compressor_pll
@@ -42,23 +67,107 @@ begin
     -- The least significant byte seemed to be sent first (manual says otherwise).
     -- Values were 10 bit.
 
-    pi_uart : entity uart
-        generic map (
-            clock_frequency => 96.0e6,
-            baud_rate => 500.0e3)
-        port map (
-            data_in => data_from_pic,
-            strobe_in => strobe_from_pic,
-            data_out => data_to_pic,
-            strobe_out => strobe_to_pic,
-            ready_out => open,
-            serial_in => rx_from_rpi,
-            serial_out => tx_to_rpi,
-            clock_in => clock);
+    leds : entity led_scan
+        port map (clock => clock,
+                  leds1_in => ad1 (7 downto 0),
+                  leds2_in => ad1 (15 downto 8),
+                  leds3_in => ad2 (7 downto 0),
+                  leds4_in => ad2 (15 downto 8),
+                  lrows_out => lrows,
+                  lcols_out => lcols);
+
+    led1 <= lrows (0);
+    led2 <= lrows (1);
+    led3 <= lrows (2);
+    led4 <= lrows (3);
+    led5 <= lrows (4);
+    led6 <= lrows (5);
+    led7 <= lrows (6);
+    led8 <= lrows (7);
+    lcol1 <= lcols (0);
+    lcol2 <= lcols (1);
+    lcol3 <= lcols (2);
+    lcol4 <= lcols (3);
+
+    process (clock)
+    begin
+        if clock'event and clock = '1' then
+            strobe_to_pic <= '0';
+            case state is
+                when RESET =>
+                    countdown <= max_countdown;
+                    state <= WAIT_BETWEEN_REQUESTS;
+
+                when WAIT_BETWEEN_REQUESTS =>
+                    if countdown = 0 then
+                        state <= SEND_REQUEST_1;
+                    else
+                        countdown <= countdown - 1;
+                    end if;
+
+                when SEND_REQUEST_1 =>
+                    data_to_pic <= std_logic_vector (to_unsigned (16#a1#, 8));
+                    strobe_to_pic <= '1';
+                    countdown <= max_countdown;
+                    state <= WAIT_REPLY_1;
+
+                when WAIT_REPLY_1 =>
+                    if strobe_from_pic = '1' then
+                        state <= WAIT_REPLY_2;
+                        ad1 (7 downto 0) <= data_from_pic;
+                    elsif countdown = 0 then
+                        state <= TIMEOUT_ERROR;
+                    else
+                        countdown <= countdown - 1;
+                    end if;
+
+                when WAIT_REPLY_2 =>
+                    if strobe_from_pic = '1' then
+                        state <= SEND_REQUEST_2;
+                        ad1 (15 downto 8) <= data_from_pic;
+                    elsif countdown = 0 then
+                        state <= TIMEOUT_ERROR;
+                    else
+                        countdown <= countdown - 1;
+                    end if;
+
+                when SEND_REQUEST_2 =>
+                    data_to_pic <= std_logic_vector (to_unsigned (16#a2#, 8));
+                    strobe_to_pic <= '1';
+                    countdown <= max_countdown;
+                    state <= WAIT_REPLY_3;
+
+                when WAIT_REPLY_3 =>
+                    if strobe_from_pic = '1' then
+                        state <= WAIT_REPLY_4;
+                        ad2 (7 downto 0) <= data_from_pic;
+                    elsif countdown = 0 then
+                        state <= TIMEOUT_ERROR;
+                    else
+                        countdown <= countdown - 1;
+                    end if;
+
+                when WAIT_REPLY_4 =>
+                    if strobe_from_pic = '1' then
+                        state <= RESET;
+                        ad2 (15 downto 8) <= data_from_pic;
+                    elsif countdown = 0 then
+                        state <= TIMEOUT_ERROR;
+                    else
+                        countdown <= countdown - 1;
+                    end if;
+
+                when TIMEOUT_ERROR =>
+                    ad1 <= std_logic_vector (to_unsigned (16#5005#, 16));
+                    ad2 <= std_logic_vector (to_unsigned (16#a00a#, 16));
+
+            end case;
+        end if;
+    end process;
 
     pic_uart : entity uart
         generic map (
-            clock_frequency => 96.0e6,
+            clock_frequency => clock_frequency,
             baud_rate => 250.0e3)
         port map (
             data_in => data_to_pic,
