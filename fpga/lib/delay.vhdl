9007355d01d824d83a7ae6944fc3fb927e060dfd
Jack Whitham <jack.d.whitham@gmail.com>
2022-08-03 19:40:42 +0100

    configure the delay size

---
+++ fpga/lib/delay.vhdl
@@ -0,0 +1,75 @@
+-- This is a configurable delay which stores 256 * num_delay1 items
+-- of size 16 bits.
+--
+-- Each input (strobe_in) will be followed by an output after 1 clock cycle
+-- provided that the delay is full.
+--
+-- Inputs are allowed every 3 clock cycles. error_out will be asserted if
+-- an input is attempted when the delay is not ready.
+
+library work;
+use work.all;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity delay is
+    generic (num_delay1 : Natural);
+    port (
+        data_in     : in std_logic_vector (15 downto 0);
+        data_out    : out std_logic_vector (15 downto 0) := (others => '0');
+        strobe_in   : in std_logic;
+        strobe_out  : out std_logic := '0';
+        error_out   : out std_logic := '0';
+        reset_in    : in std_logic;
+        clock_in    : in std_logic);
+end delay;
+
+architecture structural of delay is
+
+    constant ram_data_size        : Natural := 16;
+
+    subtype t_ram_data is std_logic_vector (ram_data_size - 1 downto 0);
+    type t_bus is record
+        data    : t_ram_data;
+        strobe  : std_logic;
+        err     : std_logic;
+        reset   : std_logic;
+    end record;
+        
+    type t_buses is array (Natural range 0 to num_delay1) of t_bus;
+    signal buses        : t_buses;
+
+begin
+    buses (0).data <= data_in;
+    buses (0).strobe <= strobe_in;
+    buses (0).err <= '0';
+    buses (0).reset <= reset_in;
+    data_out <= buses (num_delay1).data;
+    strobe_out <= buses (num_delay1).strobe;
+    error_out <= buses (num_delay1).err;
+
+    g : for i in 1 to num_delay1 generate
+        signal err : std_logic := '0';
+    begin
+        d : entity delay1
+            port map (
+                data_in => buses (i - 1).data,
+                strobe_in => buses (i - 1).strobe,
+                data_out => buses (i).data,
+                strobe_out => buses (i).strobe,
+                error_out => err,
+                reset_in => buses (i).reset,
+                clock_in => clock_in);
+
+        process (clock_in)
+        begin
+            if clock_in'event and clock_in = '1' then
+                buses (i).reset <= buses (i - 1).reset;
+                buses (i).err <= buses (i - 1).err or err;
+            end if;
+        end process;
+    end generate g;
+
+end structural;
