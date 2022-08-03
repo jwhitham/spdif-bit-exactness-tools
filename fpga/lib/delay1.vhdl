06d770dc5cd451a3e6f215ce20ba5512c2154012
Jack Whitham <jack.d.whitham@gmail.com>
2022-08-03 01:38:28 +0100

    restructured state machine; stores 256 items

--- fpga/lib/delay1.vhdl
+++ fpga/lib/delay1.vhdl
@@ -1,9 +1,9 @@
--- This is a fixed-size delay which stores 255 items of size 16 bits.
+-- This is a fixed-size delay which stores 256 items of size 16 bits.
 --
--- Each input (strobe_in) will be followed by an output after 2 clock cycles
+-- Each input (strobe_in) will be followed by an output after 1 clock cycle
 -- provided that the delay is full.
 --
--- Inputs are allowed every 2 clock cycles. error_out will be asserted if
+-- Inputs are allowed every 3 clock cycles. error_out will be asserted if
 -- an input is attempted when the delay is not ready.
 
 library ieee;
@@ -13,12 +13,13 @@ use ieee.numeric_std.all;
 use std.textio.all;
 
 entity delay1 is
+    generic (debug : Boolean := false);
     port (
         data_in     : in std_logic_vector (15 downto 0);
-        data_out    : out std_logic_vector (15 downto 0);
+        data_out    : out std_logic_vector (15 downto 0) := (others => '0');
         strobe_in   : in std_logic;
-        strobe_out  : out std_logic;
-        error_out   : out std_logic;
+        strobe_out  : out std_logic := '0';
+        error_out   : out std_logic := '0';
         reset_in    : in std_logic;
         clock_in    : in std_logic);
 end delay1;
@@ -28,17 +29,16 @@ architecture structural of delay1 is
     constant ram_data_size        : Natural := 16;
     constant ram_addr_size        : Natural := 8;
 
-    subtype t_ram_addr is std_logic_vector (ram_addr_size - 1 downto 0);
+    subtype t_ram_addr is std_logic_vector (ram_addr_size downto 0);
     subtype t_ram_data is std_logic_vector (ram_data_size - 1 downto 0);
 
-    type t_state is (READY, ADVANCE_ADDR, READ_DATA, WRITE_OUT);
+    type t_state is (READ, WRITE, ADVANCE);
 
     constant one            : std_logic := '1';
     constant mask           : t_ram_data := (others => '0');
     constant zero_addr      : t_ram_addr := (others => '0');
     signal addr             : t_ram_addr := (others => '0');
-    signal state            : t_state := READY;
-    signal refilled         : std_logic := '0';
+    signal state            : t_state := READ;
     signal write_enable     : std_logic := '0';
     signal read_enable      : std_logic := '0';
     signal data_gen         : t_ram_data := (others => '0');
@@ -69,53 +69,55 @@ begin
     begin
         if clock_in'event and clock_in = '1' then
             error_out <= '0';
-            strobe_out <= '0';
             case state is
-                when READY =>
+                when READ =>
+                    assert strobe_in = '0';
+                    error_out <= strobe_in;
+                    state <= WRITE;
+
+                when WRITE =>
                     if strobe_in = '1' then
-                        write (l, String'("write value "));
-                        write (l, to_integer (unsigned (data_in)));
-                        write (l, String'(" to address "));
-                        write (l, to_integer (unsigned (addr)));
-                        writeline (output, l);
-                        state <= ADVANCE_ADDR;
+                        if debug then
+                            write (l, String'("write value "));
+                            write (l, to_integer (unsigned (data_in)));
+                            write (l, String'(" to address "));
+                            write (l, to_integer (unsigned (addr (addr'Left - 1 downto 0))));
+                            writeline (output, l);
+                        end if;
+                        state <= ADVANCE;
                     end if;
-                when ADVANCE_ADDR =>
+
+                when ADVANCE =>
                     assert strobe_in = '0';
                     error_out <= strobe_in;
+
+                    -- address advances
                     addr <= std_logic_vector (unsigned (addr) + 1);
-                    state <= READ_DATA;
-                when READ_DATA =>
-                    assert strobe_in = '0';
-                    error_out <= strobe_in;
-                    if addr = zero_addr then
-                        refilled <= '1';
+
+                    -- keep top bit of addr '1' once set (indicates delay is full)
+                    if addr (addr'Left) = '1' then
+                        addr (addr'Left) <= '1';
+                        if debug then
+                            write (l, String'("read value "));
+                            write (l, to_integer (unsigned (data_gen)));
+                            write (l, String'(" from address "));
+                            write (l, to_integer (unsigned (addr (addr'Left - 1 downto 0))));
+                            writeline (output, l);
+                        end if;
                     end if;
-                    state <= WRITE_OUT;
-                when WRITE_OUT =>
-                    assert strobe_in = '0';
-                    error_out <= strobe_in;
-                    strobe_out <= refilled;
-                    if refilled = '1' then
-                        write (l, String'("read value "));
-                        write (l, to_integer (unsigned (data_gen)));
-                        write (l, String'(" from address "));
-                        write (l, to_integer (unsigned (addr)));
-                        writeline (output, l);
-                        state <= ADVANCE_ADDR;
-                    end if;
-                    state <= READY;
+                    state <= READ;
             end case;
             if reset_in = '1' then
-                state <= READY;
-                refilled <= '0';
-                addr <= zero_addr;
+                state <= READ;
+                addr <= (others => '0');
+                error_out <= '0';
             end if;
         end if;
     end process generate_addr;
 
-    write_enable <= strobe_in when state = READY else '0';
-    read_enable <= '1' when state = READ_DATA else '0';
+    strobe_out <= addr (addr'Left) when state = ADVANCE else '0';
+    write_enable <= '1' when state = WRITE else '0';
+    read_enable <= '1' when state = READ else '0';
     data_out <= data_gen;
 
     ram : SB_RAM40_4K
@@ -123,13 +125,13 @@ begin
             READ_MODE => 0,
             WRITE_MODE => 0)
         port map (
-            WADDR => addr,
+            WADDR => addr (addr'Left - 1 downto 0),
             WDATA => data_in,
             MASK => mask,
             WE => write_enable,
             WCLKE => one,
             WCLK => clock_in,
-            RADDR => addr,
+            RADDR => addr (addr'Left - 1 downto 0),
             RDATA => data_gen,
             RE => read_enable,
             RCLKE => one,
--- fpga/test/test_delay1.vhdl
+++ fpga/test/test_delay1.vhdl
@@ -14,6 +14,7 @@ architecture test of test_delay1 is
     subtype t_data is std_logic_vector (15 downto 0);
 
     signal data_in     : t_data := (others => '0');
+    signal data_in_copy: t_data := (others => '0');
     signal data_out    : t_data := (others => '0');
     signal strobe_in   : std_logic := '0';
     signal strobe_out  : std_logic := '0';
@@ -23,9 +24,10 @@ architecture test of test_delay1 is
     signal done        : std_logic := '0';
     signal expect_output : std_logic := '0';
 
-    constant first_value : Natural := 1234;
-    constant last_value : Natural := 2234;
-    constant delay_size : Natural := 255;
+    constant first_value : Natural := 2000;
+    constant last_value : Natural := 3000;
+    constant incorrect_value : Natural := 31337;
+    constant delay_size : Natural := 256;
 
 begin
     -- 1MHz clock (one clock every 1000ns)
@@ -51,18 +53,23 @@ begin
             reset_in => reset_in,
             clock_in => clock_in);
 
-    -- assert error_out = '0';
+    assert error_out = '0';
 
     signal_generator : process
+        variable l : line;
     begin
         reset_in <= '1';
         done <= '0';
         strobe_in <= '0';
         expect_output <= '0';
+        data_in <= std_logic_vector (to_unsigned (incorrect_value, 16));
         wait until clock_in = '0';
         wait for 10 us;
         reset_in <= '0';
         wait for 10 us;
+
+        write (l, String'("test delay"));
+        writeline (output, l);
 
         -- Fill with test data
         for i in first_value to last_value loop
@@ -70,13 +77,12 @@ begin
                 expect_output <= '1';
             end if;
             data_in <= std_logic_vector (to_unsigned (i, 16));
+            data_in_copy <= std_logic_vector (to_unsigned (i, 16));
             strobe_in <= '1';
             wait for 1 us;
-            data_in <= std_logic_vector (to_unsigned (i + 1000, 16));
+            data_in <= std_logic_vector (to_unsigned (incorrect_value, 16));
             strobe_in <= '0';
-            wait for 1 us;
-            data_in <= std_logic_vector (to_unsigned (0, 16));
-            wait for 3 us;
+            wait for 2 us;
         end loop;
 
         wait for 100 us;
@@ -89,9 +95,8 @@ begin
         variable i : Natural := first_value;
     begin
         -- Check test data
-        loop
+        while done = '0' loop
             wait until strobe_out'event or done'event;
-            assert done = '0';
             if strobe_out'event and strobe_out = '1' then
                 assert expect_output = '1';
                 if data_out /= std_logic_vector (to_unsigned (i, 16)) then
@@ -100,6 +105,17 @@ begin
                     write (l, String'(" expected "));
                     write (l, i);
                     writeline (output, l);
+                    assert False;
+                end if;
+                if (unsigned (data_out) + to_unsigned (delay_size, 16)) /= unsigned (data_in_copy) then
+                    write (l, String'("output from delay "));
+                    write (l, to_integer (unsigned (data_out)));
+                    write (l, String'(" is not "));
+                    write (l, delay_size);
+                    write (l, String'(" behind the input "));
+                    write (l, to_integer (unsigned (data_in_copy)));
+                    writeline (output, l);
+                    assert False;
                 end if;
                 assert data_out = std_logic_vector (to_unsigned (i, 16));
                 wait for 1001 ns;
@@ -107,12 +123,11 @@ begin
                 assert data_out = std_logic_vector (to_unsigned (i, 16));
                 i := i + 1;
             end if;
-            exit when i >= (last_value - delay_size + 1);
         end loop;
+        assert i = (last_value - delay_size + 1);
 
-        wait until strobe_out'event or done'event;
-        assert done = '1';
-        assert strobe_out = '0';
+        write (l, String'("delay operated as expected"));
+        writeline (output, l);
         wait;
     end process check_data;
 
